64527d192af0b51fae3bacd985a3ad2217d48928
joycyrus <isouvede2@gmail.com>
2022-02-07 19:15:06 +0200

    added store products and display them on homepage

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,8 +1,14 @@
-from turtle import home
+# from turtle import home
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from store.models import Product
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all().filter(is_available = True)
+    
+    context = {
+        'products' : products
+    }
+    return render(request, 'home.html',context)
   
