a4ed081c12f4ea2a3e144a0fe0161b70c01519f9
joycyrus <isouvede2@gmail.com>
2022-02-07 20:22:55 +0200

    displayed db product on store endpoint with count

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -23,5 +23,6 @@ from greatkart.settings import MEDIA_ROOT
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home, name='home')
+    path('',views.home, name='home'),
+    path('store/',include('store.urls'), name='store'),
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,13 @@
+from itertools import product
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def store(request):
+    products = Product.objects.all().filter(is_available=True)
+    products_count = products.count()
+    context = {
+        'products':products,
+        'count':products_count
+    }
+    return render(request, 'store/store.html',context)
