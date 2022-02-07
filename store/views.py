75924b8cd9b32cbdf3077169be0de88226c5c491
joycyrus <isouvede2@gmail.com>
2022-02-07 20:54:32 +0200

    displayed db product per category on an endpoint

--- store/views.py
+++ store/views.py
@@ -1,11 +1,22 @@
-from itertools import product
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from category.models import Category
 from .models import Product
 
 # Create your views here.
-def store(request):
-    products = Product.objects.all().filter(is_available=True)
-    products_count = products.count()
+
+def store(request, category_slug = None):
+    categories = None
+    products = None
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug= category_slug )
+        print(categories)
+        products = Product.objects.filter(category = categories, is_available=True)
+        products_count = products.count()
+        
+    else:
+        products = Product.objects.all().filter(is_available= True)
+        products_count = products.count()
     context = {
         'products':products,
         'count':products_count
