f411001dd69268a8f444b493ace20aed14693db3
joycyrus <isouvede2@gmail.com>
2022-02-11 21:29:15 +0200

    add to cart functionality with contextprocessor for cart count

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from tokenize import single_quoted
-from urllib import request
+from django import views
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from carts.models import Cart, CartItem
+from carts.views import _cart_id
 
 from category.models import Category
 from .models import Product
@@ -27,13 +30,19 @@ def store(request, category_slug = None):
 
 def product_detail(request,category_slug,product_slug):
     try:
-        #get object->category->slug syntax
+        #get object->category->slug syntax use __
         single_product = Product.objects.get(category__slug = category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id = _cart_id(request), product = single_product).exists()
+        
+        #testing results
+        # return HttpResponse(in_cart)
+        # exit()
     except Exception as e:
         return e
     
     context = {
-        'single_product':single_product
+        'single_product':single_product, 
+        'in_cart':in_cart
     }
         
     return render(request,'store/product_detail.html',context)
